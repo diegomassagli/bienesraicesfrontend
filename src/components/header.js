f076cee98ac051a6b4071af7cab96d04773dbfd1
Diego Massagli <diegomassagli@gmail.com>
2022-03-12 17:37:28 -0300

    a mitad del 541 antes del error de link

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+//import { Link } from 'gatsby';
+
+
+const Header = () => {
+  return ( 
+
+    <Header>
+      {/* <div>
+        <Link to="/">
+          Bienes Raices
+        </Link>
+      </div> */}
+    </Header>
+
+   );
+}
+ 
+export default Header;
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-
+import { Global, css }  from '@emotion/react';
+// import Header from './header';
 
 const Layout = ({ children }) => {
   return ( 
     <>
+    <Global
+      styles={css`
+        html {
+          font-size: 62.5%;
+          box-sizing: border-box;
+        }
+        *, *:before, *:after {
+          box-sizing: inherit;
+        }
+        body{
+          font-size: 1.6rem; //equivale a 16px
+          line-height: 2;
+          font-family: 'Lato', sans-serif;
+          h1,h2,h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+          h1,h2 {
+            text-align: center;
+            font-family: 'Lato', sans-serif;
+            font-weight: 300;
+          }
+          h3{
+            font-family: 'Roboto', sans-serif;
+          }
+          ul{
+            list-style: none;
+            margin:0;
+            padding: 0;
+          }
+          .contenedor{
+            max-width: 1200px; //o 120rem
+            margin: 0 auto;
+            width: 95%;
+          }
+          img{
+            max-width: 100%; //para hacerlas responsive. asi nunca se achica pero nunca crece mas que su tamaño original. con width:100% + height: auto puede crecer y perder calidad
+          }
+        }
+      `}
+    />
     <Helmet>
         <title>Bienes Raices Gatsby</title>
         <meta name="description" content="Sitio web de bienes raices en Gatsby" />
@@ -13,7 +55,10 @@ const Layout = ({ children }) => {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
     </Helmet>
-      {children}
+    
+    {/* <Header /> */}
+
+    {children}
     </>
    );
 }
